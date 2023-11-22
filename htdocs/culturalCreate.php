f926aa2bf4abd9319c80f2f1f4c5673256bbfbab
RootToBranch <stdodentysoung@gmail.com>
2023-11-22 20:51:34 +0900

    Page 'culturalCreate' Complete

--- htdocs/culturalCreate.php
+++ htdocs/culturalCreate.php
@@ -21,28 +21,29 @@
                     <label><span>종목코드 *</span> <input type="number" name="1" required></label>
                     <label><span>지정번호 *</span> <input type="number" name="2" required></label>
                     <label><span>시도코드 *</span> <input type="number" name="3" required></label>
-                    <label><span>경도</span> <input type="text" name="4"></label>
-                    <label><span>위도</span> <input type="text" name="5"></label>
-                    <label><span>문화재종목 *</span> <input type="text" name="6" required></label>
-                    <label><span>지정호수 *</span> <input type="text" name="7" required></label>
-                    <label><span>문화재명(국문) *</span> <input type="text" name="8" required></label>
-                    <label><span>문화재명(한자)</span> <input type="text" name="9"></label>
-                    <label><span>문화재분류</span> <input type="text" name="10"></label>
-                    <label><span>문화재분류2(종류)</span> <input type="text" name="11"></label>
-                    <label><span>문화재분류3</span> <input type="text" name="12"></label>
-                    <label><span>문화재분류4</span> <input type="text" name="13"></label>
-                    <label><span>수량</span> <input type="text" name="14"></label>
-                    <label><span>지정(등록일)</span> <input type="date" name="15"></label>
-                    <label><span>시도명</span> <input type="text" name="16"></label>
-                    <label><span>시군구명</span> <input type="text" name="17"></label>
-                    <label><span>소재지 상세</span> <input type="text" name="18"></label>
-                    <label><span>시대</span> <input type="text" name="19"></label>
-                    <label><span>소유자</span> <input type="text" name="20"></label>
-                    <label><span>관리자</span> <input type="text" name="21"></label>
-                    <label><span>지정해제여부</span> <input type="text" name="22"></label>
-                    <label><span>지정해제일</span> <input type="text" name="23"></label>
-                    <label><span>이미지</span> <input type="file" name="24"></label>
-                    <label><span>설명</span> <input type="text" name="25"></label>
+                    <label><span>연계번호</span> <input type="text" name="4"></label>
+                    <label><span>경도</span> <input type="text" name="5"></label>
+                    <label><span>위도</span> <input type="text" name="6"></label>
+                    <label><span>문화재종목 *</span> <input type="text" name="7" required></label>
+                    <label><span>지정호수 *</span> <input type="text" name="8" required></label>
+                    <label><span>문화재명(국문) *</span> <input type="text" name="9" required></label>
+                    <label><span>문화재명(한자)</span> <input type="text" name="10"></label>
+                    <label><span>문화재분류</span> <input type="text" name="11"></label>
+                    <label><span>문화재분류2(종류)</span> <input type="text" name="12"></label>
+                    <label><span>문화재분류3</span> <input type="text" name="13"></label>
+                    <label><span>문화재분류4</span> <input type="text" name="14"></label>
+                    <label><span>수량</span> <input type="text" name="15"></label>
+                    <label><span>지정(등록일)</span> <input type="date" name="16"></label>
+                    <label><span>시도명</span> <input type="text" name="17"></label>
+                    <label><span>시군구명</span> <input type="text" name="18"></label>
+                    <label><span>소재지 상세</span> <input type="text" name="19"></label>
+                    <label><span>시대</span> <input type="text" name="20"></label>
+                    <label><span>소유자</span> <input type="text" name="21"></label>
+                    <label><span>관리자</span> <input type="text" name="22"></label>
+                    <label><span>지정해제여부</span> <input type="text" name="23"></label>
+                    <label><span>지정해제일</span> <input type="text" name="24"></label>
+                    <label><span>이미지</span> <input type="file" name="25"></label>
+                    <label><span>설명</span> <input type="text" name="26"></label>
                     <button type="submit">등록</button>
                 </form>
 
--- htdocs/culturalCreate_process.php
+++ htdocs/culturalCreate_process.php
@@ -1,62 +1,42 @@
 <?php
+    define("__root", $_SERVER["DOCUMENT_ROOT"]);
+    define("Core", __root . "/Core");
 
-    require_once "DB.php";
-    foreach ($_POST as $key => $value) {
-        $sql = "INSERT INTO `nihdetail`(
-            `ccbaKdcd`,
-            `ccbaAsno`,
-            `ccbaCtcd`,
-            `ccbaCpno`,
-            `longitude`,
-            `latitude`,
-            `ccmaName`,
-            `crltsnoNm`,
-            `ccbaMnm1`,
-            `ccbaMnm2`,
-            `gcodeName`,
-            `bcodeName`,
-            `mcodeName`,
-            `scodeName`,
-            `ccbaQuan`,
-            `ccbaAsdt`,
-            `ccbaCtcdNm`,
-            `ccsiName`,
-            `ccbaLcad`,
-            `ccceName`,
-            `ccbaPoss`,
-            `ccbaAdmin`,
-            `ccbaCncl`,
-            `ccbaCndt`,
-            `imageUrl`,
-            `content`
-        )
-        VALUES(
-            '[value-1]',
-            '[value-2]',
-            '[value-3]',
-            '[value-4]',
-            '[value-5]',
-            '[value-6]',
-            '[value-7]',
-            '[value-8]',
-            '[value-9]',
-            '[value-10]',
-            '[value-11]',
-            '[value-12]',
-            '[value-13]',
-            '[value-14]',
-            '[value-15]',
-            '[value-16]',
-            '[value-17]',
-            '[value-18]',
-            '[value-19]',
-            '[value-20]',
-            '[value-21]',
-            '[value-22]',
-            '[value-23]',
-            '[value-24]',
-            '[value-25]',
-            '[value-26]'
-        )";
-        // execute($sql)
+    require_once Core . "/db.php";
+    require_once Core . "/helper.php";
+
+    if($_FILES[25]['tmp_name'] != "" && 
+        substr(($_FILES[25]['type']), 0, 5) != "image") {
+        back('이미지 파일만 선택할 수 있습니다.');
     }
+    $temp = array_pop($_POST);
+    $_POST[25] = $_FILES[25];
+    $_POST[26] = $temp;
+
+    $sql = "INSERT INTO `nihdetail` VALUES(";
+    foreach ($_POST as $key => $value) {
+        $value_2 = "";
+        if($key == 25) {
+            if($_POST[25]['full_path'] != "") {
+                $fileName = uniqid('', true) . '_'. $_POST[25]['name'];
+                $filePath = "../nihcImage/$fileName";
+                $value_2 = $fileName;
+    
+                move_uploaded_file($_POST[25]['tmp_name'], $filePath);
+            } else $value_2 = 'null';
+        } else {
+            // var_dump($value == '');
+            $value_2 = ($value) == '' ? 'null' : $value;
+        }
+
+        $sql .= $value_2 == 'null' ? "null" : "'$value_2'";
+        if($key < count($_POST)) $sql .= ", ";
+    }
+    $sql .= ");";
+
+    var_dump($sql);
+    execute($sql);
+    // go('/culturalProperties.php', "정상적으로 등록되었습니다.");
+    exit;
+    // var_dump($_POST);
+    // execute($sql);
